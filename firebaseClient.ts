fe08547ec5e5d4c71f2f9e22ca6c39087010c728
hujimori16 <hujimori16@gmail.com>
2023-04-10 00:13:21 +0900

    config修正

--- firebaseClient.ts
+++ firebaseClient.ts
@@ -8,13 +8,13 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCd0zkX5dwe17R_CUjcoMeuLzi3CJi3-pg',
-  authDomain: 'ec-app-b9e3f.firebaseapp.com',
-  projectId: 'ec-app-b9e3f',
-  storageBucket: 'ec-app-b9e3f.appspot.com',
-  messagingSenderId: '966916761202',
-  appId: '1:966916761202:web:25f86996d6c548a92604eb',
-  measurementId: 'G-G7XBVPN0DR',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 let app: FirebaseApp | null;
